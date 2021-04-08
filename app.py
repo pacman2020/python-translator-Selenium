6e3ba5405ebe2622f79027c4ff9f74e743066e3d
geraldo <geeferreirasd034@gmail.com>
2021-04-08 16:07:45 -0300

    structuring code

--- app.py
+++ app.py
@@ -2,16 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+url = 'https://translate.google.com.br/?sl=pt&tl=en&op=translate'
+
+texto = 'casa nova'
+
 driver = webdriver.Firefox(executable_path='./geckodriver')
-driver.get('https://translate.google.com.br/?sl=pt&tl=en&op=translate')
+driver.get(url)
 
-#search
+#campo de entradas de texto
 elent = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[1]/span/span/div/textarea')
-elent.send_keys('casa nova')
 
-time.sleep(3)
+#inserido texto a ser traduzindo
+elent.send_keys(texto)
 
 #ante de pega o valor deve espera ate esta pronto
+time.sleep(4)
+
+#campo de texto traduzindo
 outside = driver.find_element_by_xpath('/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[2]/c-wiz[2]/div[5]/div/div[1]').text
 
 #guarda a informação onde quizer
