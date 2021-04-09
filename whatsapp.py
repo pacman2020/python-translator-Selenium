c7a899dc20115eea618ae9a4d09db4f4dec668f6
geraldo <geeferreirasd034@gmail.com>
2021-04-09 18:54:58 -0300

    creating a list with group names or people to send a message until the names in the list run out

--- whatsapp.py
+++ whatsapp.py
@@ -4,24 +4,25 @@ from time import sleep
 
 url = 'https://web.whatsapp.com/'
 
-texto = ['Pacman-2020']
-
 driver = webdriver.Firefox(executable_path='./geckodriver')
 driver.get(url)
 
 sleep(7)
-search = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]')
+grupos = ['Pacman-2020', 'Gaby']
 
-sleep(3)
-search.send_keys('Pacman-2020')
+for name in grupos:
+    
+    search = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]')
+    sleep(3)
+    search.send_keys(name)
+    sleep(2)
+    search.send_keys(Keys.RETURN)
 
-sleep(2)
-search.send_keys(Keys.RETURN)
-
-sleep(3)
-campo = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]')
-sleep(2)
-campo.send_keys('teste bot')
-sleep(2)
-campo.send_keys(Keys.RETURN)
-# driver.close()
+    sleep(3)
+    campo = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]')
+    sleep(2)
+    campo.send_keys('geraldo testando algo kkk')
+    sleep(2)
+    campo.send_keys(Keys.RETURN)
+    
+driver.close()
