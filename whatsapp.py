a1b5cb5a2e6a308d5167ef1e11d3ebbd4544a301
geraldo <geeferreirasd034@gmail.com>
2021-04-09 18:43:27 -0300

    searching the sending field and sending a message to the group

--- whatsapp.py
+++ whatsapp.py
@@ -9,7 +9,7 @@ texto = ['Pacman-2020']
 driver = webdriver.Firefox(executable_path='./geckodriver')
 driver.get(url)
 
-sleep(2)
+sleep(7)
 search = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[3]/div/div[1]/div/label/div/div[2]')
 
 sleep(3)
@@ -17,4 +17,11 @@ search.send_keys('Pacman-2020')
 
 sleep(2)
 search.send_keys(Keys.RETURN)
+
+sleep(3)
+campo = driver.find_element_by_xpath('/html/body/div/div[1]/div[1]/div[4]/div[1]/footer/div[1]/div[2]/div/div[2]')
+sleep(2)
+campo.send_keys('teste bot')
+sleep(2)
+campo.send_keys(Keys.RETURN)
 # driver.close()
